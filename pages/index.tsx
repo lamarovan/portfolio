5c0fec08c9dd7a0925801f5833f15c2a7fe64769
rovanilla <rovanlama@live.com>
2023-05-23 11:27:04 +0545

    Title added

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import { WordAnimation } from '../components/wordanimation/wordanimation';
 import styles from '../components/index.module.css';
 import Layout from '@/components/layout';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,6 +12,10 @@ const word = 'fullstack developer';
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <title>Hi, its Rov</title>
+      </Head>
+      {/* hero section */}
       <div className='bg-pale'>
         <section className='content'>
           <div className={styles.header}>
@@ -30,6 +35,17 @@ export default function Home() {
             </h1>
           </div>
         </section>
+      </div>
+
+      {/* tech stack */}
+      <div className='tech-stack-section'>
+        <div className='content'>
+          <div className={styles.header}>
+            <p className={styles.subtitle}>
+              I have experience with the following tech stacks
+            </p>
+          </div>
+        </div>
       </div>
     </Layout>
   );
